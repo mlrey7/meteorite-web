94a0eea9e002c8a7ca66de6a2ab65cba190c2382
Matthew Lemuel M Rey <matthewlemuelr@gmail.com>
2019-11-19 22:54:22 +0800

    User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'welcome#index'
   get 'home/index'
   devise_for :users
-  root to: 'welcome#index'
+  resources :users, :only => [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
