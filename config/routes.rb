e175a380cf8d08880a15c5b0d5feff5d0d674371
Matthew Lemuel M Rey <matthewlemuelr@gmail.com>
2019-11-20 06:23:19 +0800

    Microposts feed at user show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'home/index'
   devise_for :users
-  resources :users, :only => [:show]
+  resources :users, :only: [:show]
+  resources :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
