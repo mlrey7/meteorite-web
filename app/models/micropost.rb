d1370727a30bf4617e001ba8ee20642e930e1821
Matthew Lemuel M Rey <matthewlemuelr@gmail.com>
2019-11-20 23:29:57 +0800

    picture size remove

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,12 +4,4 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 300 }
-  validates picture_size
-
-  private
-    def picture_size
-      if picture.size > 5.megabytes
-        errors.add(:picture, "should be less than 5MB")
-      end
-   end
 end
